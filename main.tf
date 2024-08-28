7b43363b3c68dde73fc30431eb1bc9fa3d48b42d
Lynda Nwabudike <lynda.holivia@gmail.com>
2024-08-28 14:05:09 +0100

    cicd

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.52.0"
+      source  = "hashicorp/aws"
+    }
+  }
+  required_version = "~> 1.1.9"
+}
+
 provider "aws" {
   region = var.aws_region
 }
@@ -5,6 +15,22 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  metadata_options {
+     http_tokens = "required"
+     } 
+
+  root_block_device {
+      encrypted = true
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sdg"
+    volume_size = 5
+    volume_type = "gp2"
+    delete_on_termination = false
+    encrypted = true
+  }
+   
 
   tags = {
     Name = "TerraformExampleInstance"
